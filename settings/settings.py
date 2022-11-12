feda3f8bf297303fdffa1ccd800cf97c7bc648ca
estepina <este@DESKTOP-AVSP69B.localdomain>
2022-11-12 15:26:51 +0300

    routed urls

--- settings/settings.py
+++ settings/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drills.apps.DrillsConfig",
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
