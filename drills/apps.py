1ed0f6b5d0f6198b732ccf1782ea7d8bc92ecfe1
estepina <este@DESKTOP-AVSP69B>
2023-09-11 14:21:20 +0300

    project restart

--- drills/apps.py
+++ drills/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DrillsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'drills'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "drills"
