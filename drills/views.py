f0f2b25726e9fde683d5211d01ba36a956112d41
estepina <este@DESKTOP-AVSP69B>
2023-09-12 16:12:44 +0300

    created base templates

--- drills/views.py
+++ drills/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    context = {}
+    return render(request, "index.html", context)
--- settings/urls.py
+++ settings/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("drills/", include("drills.urls")),
+    path("", include("drills.urls")),
 ]
