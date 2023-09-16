67fa1a0c869d56bcdb95da14305c7c25589d8d95
estepina <este@DESKTOP-AVSP69B>
2023-09-16 17:01:59 +0300

    created drill model

--- drills/models.py
+++ drills/models.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Drill(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="drills")
